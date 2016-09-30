d49d3cb7e931f72856bef25df771940734008651
Possardt <rpossardt@gmail.com>
2016-09-30 13:04:32 -0400

    added two sections to fantasyemail

--- EmailSender.py
+++ EmailSender.py
@@ -11,7 +11,7 @@ msgHTML = """\
 <!-- Change this style tag to inline eventually -->
 
 <body>
-<table style="width: 500px;table-layout:fixed;border-spacing:0;font:20px Arial, sans-serif;">
+<table align="center" style="width: 500px;table-layout:fixed;border-spacing:0;font:20px Arial, sans-serif;">
 <tbody>
 	<tr>
 		<th width="100%" style="background-color: lightgreen; font-size:50px;">
@@ -23,24 +23,25 @@ msgHTML = """\
 	
 	<!-- Most points scored rows:-->
 	<tr>
-		<td style="float:left;"><strong>Most Points Scored:</strong></td>
+		<td style="float:left;"><strong>Most Points Scored:</strong><div style="font-size:10px">(This week)</div></td>
 	</tr>
 	<tr>
 		<td style="float:left;" width="10%"></td>
 		<td style="float:left;" width="50%">TeamName</td>
 		<td style="float:left;"><div style="border-radius:50%;	width: 50px;height: 50px;padding: 8px;background: lightgreen;border: 0px solid #666;color: #666;text-align: center;font: 32px Arial, sans-serif;">100</div></td>
 	</tr>
+	<tr style="background-color: white;height:5px;"><td></td></tr>
 	
 	<!--Least points scored rows: -->
 	<tr style="background-color:lightgreen;margin-top:10px">
-		<td style="float:left;"><strong>Least Points Scored: </strong></td>
+		<td style="float:left;"><strong>Least Points Scored: </strong><div style="font-size:10px">(This week)</div></td>
 	</tr>
 	<tr style="background-color:lightgreen">
 		<td style="float:left;" width="10%"></td>
 		<td style="float:left;" width="50%">TeamName</td>
 		<td style="float:left;"><div style="border-radius:50%;	width: 50px;height: 50px;padding: 8px;background: white;border: 0px solid #666;color: #666;text-align: center;font: 32px Arial, sans-serif;">30</div></td>
 	</tr>
-	
+	<tr style="background-color: lightgreen;height:5px;"><td></td></tr>
 	
 	<!-- league standings -->
 	<tr>
@@ -79,7 +80,32 @@ msgHTML = """\
 		<td style="float:left;" width="30%"></td>
 		<td style="float:left;">4. Team</td>
 	</tr>
+	<tr style="background-color: white;height:5px;"><td></td></tr>
+
+	<!--Most points scored so far -->
+	<tr style="background-color:lightgreen;">
+		<td style="float:left;"><strong>Overall Scoring Leader:</strong></td>
+	</tr>
+	<tr style="background-color:lightgreen">
+		<td style="float:left;" width="10%"></td>
+		<td style="float:left;" width="50%">TeamName</td>
+		<td style="float:left;"><div style="border-radius:50%;	width: 70px;height: 70px;padding: 8px;background: white;border: 0px solid #666;color: #666;text-align: center;vertical-align:center;font: 32px Arial, sans-serif;">500</div></td>
+	</tr>
+	<tr style="background-color: lightgreen;height:5px;"><td></td></tr>
+
+	<!--Least points scored so far -->
+	<tr>
+		<td style="float:left;"><strong>Overall Scoring Loser:</strong></td>
+	</tr>
+	<tr>
+		<td style="float:left;" width="10%"></td>
+		<td style="float:left;" width="50%">TeamName</td>
+		<td style="float:left;"><div style="border-radius:50%;	width: 70px;height: 70px;padding: 8px;background: lightgreen;border: 0px solid #666;color: #666;text-align: center;font: 32px Arial, sans-serif;">100</div></td>
+	</tr>
+	<tr style="background-color: white;height:5px;"><td></td></tr>
 	
+
+
 	<!--Bottom row -->
 	<tr style="background-color:lightgreen; font-size: 12px;">
 		<td style="float:left;">Copyright Ryan Possardt</td>
@@ -102,7 +128,7 @@ msg = MIMEText(msgHTML, 'html')
 
 
 
-msg['Subject'] = Header('Test7', 'utf-8')
+msg['Subject'] = Header('Test8', 'utf-8')
 msg['From'] = login
 msg['To'] = ", ".join(recipients)
 
