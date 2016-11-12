adbc3af9d7c35b1c75bfb44645d4587ca5456495
Ryan Possardt <rpossardt@gmail.com>
2016-11-12 14:26:38 -0500

    minor changes

--- EmailSender.py
+++ EmailSender.py
@@ -10,16 +10,16 @@ from StaticContent import LeagueInfo
 
 login, password = 'rpossardt@gmail.com', getpass()
 testEmail = 'rpossardt@gmail.com'
-week = 6
+week = 9
 
 # create BTB message
-msg = MIMEText(getMessageHTML(week, LeagueInfo.sectionTitlesBTB , LeagueInfo.leagueScoreboardURLBTB , LeagueInfo.leagueStandingsURLBTB), 'html')
-msg['Subject'] = Header(LeagueInfo.sectionTitlesBTB[0] + ' Week ' + str(week), 'utf-8')
-msg['From'] = login
-msg['To'] = ', '.join(LeagueInfo.leagueEmailsBTB)
+# msg = MIMEText(getMessageHTML(week, LeagueInfo.sectionTitlesBTB , LeagueInfo.leagueScoreboardURLBTB , LeagueInfo.leagueStandingsURLBTB, LeagueInfo.leagueIDBTB), 'html')
+# msg['Subject'] = Header(LeagueInfo.sectionTitlesBTB[0] + ' Week ' + str(week), 'utf-8')
+# msg['From'] = login
+# msg['To'] = ', '.join(LeagueInfo.leagueEmailsBTB)
 
 # create TFF message
-msg1 = MIMEText(getMessageHTML(week, LeagueInfo.sectionTitlesTFF , LeagueInfo.leagueScoreboardURLTFF , LeagueInfo.leagueStandingsURLTFF), 'html')
+msg1 = MIMEText(getMessageHTML(week, LeagueInfo.sectionTitlesTFF , LeagueInfo.leagueScoreboardURLTFF , LeagueInfo.leagueStandingsURLTFF, LeagueInfo.leagueIDTFF), 'html')
 msg1['Subject'] = Header(LeagueInfo.sectionTitlesTFF[0] + ' Week ' + str(week),'utf-8')
 msg1['From'] = login
 msg1['To'] = ', '.join(LeagueInfo.leagueEmailsTFF)
@@ -30,7 +30,7 @@ s = SMTP_SSL('smtp.gmail.com', 465, timeout=10)
 s.set_debuglevel(1)
 try:
     s.login(login, password)
-    s.sendmail(msg['From'], LeagueInfo.leagueEmailsBTB, msg.as_string())
+    #s.sendmail(msg['From'], LeagueInfo.leagueEmailsBTB, msg.as_string())
     s.sendmail(msg1['From'], LeagueInfo.leagueEmailsTFF, msg1.as_string())
 finally:
     s.quit()
